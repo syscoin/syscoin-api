4a5391d6c7e59a4b6d71c31c118561ac13e41271
Dan Wasyluk <dwasyluk@gmail.com>
2014-09-13 20:13:59 -0400

    addnode thru aliasupdate
    addnode <node> <add|remove|onetry>
    aliasactivate <alias> <rand> [<tx>] <value>
    aliasclean
    aliasfilter [[[[[regexp] maxage=36000] from=0] nb=0] stat]
    aliashistory <name>
    aliasinfo <name>
    aliaslist [<name>]
    aliasnew <name>
    aliasscan [<start-name>] [<max-returned>]
    aliasupdate <alias> <value> [<toaddress>]

--- server.js
+++ server.js
@@ -46,6 +46,96 @@ router.get('/addmultisigaddress', function(req, res) {
     });
 });
 
+router.get('/addnode', function(req, res) {
+    console.log('addnode(' + req.query.node + ', ' + req.query.method + ')');
+    sysclient.addnode(req.query.node, req.query.method, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliasactivate', function(req, res) {
+    console.log('aliasactivate(' + req.query.aliasName + ', ' + req.query.guid + ', ' + req.query.tx + ', ' + req.query.value + ')');
+    sysclient.aliasactivate(req.query.aliasName, req.query.guid, req.query.tx, req.query.value, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliasclean', function(req, res) {
+    console.log('aliasclean()');
+    sysclient.aliasclean(function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliasfilter', function(req, res) {
+    console.log('aliasfilter(' + req.query.regexp + ', ' + req.query.maxage + ', ' + req.query.from + ', ' + req.query.nb + ', ' + req.query.stat + ')');
+    sysclient.aliasfilter(req.query.regexp, req.query.maxage, req.query.from, req.query.nb, req.query.stat, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliashistory', function(req, res) {
+    console.log('aliashistory(' + req.query.aliasName + ')');
+    sysclient.aliashistory(req.query.aliasName, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliasinfo', function(req, res) {
+    console.log('aliasinfo(' + req.query.aliasName + ')');
+    sysclient.aliasinfo(req.query.aliasInfo, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliaslist', function(req, res) {
+    console.log('aliaslist(' + req.query.aliasNameFilter + ')');
+    sysclient.aliaslist(req.query.aliasNameFilter, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliasnew', function(req, res) {
+    console.log('aliasnew(' + req.query.aliasName + ')');
+    sysclient.aliasnew(req.query.aliasName, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliasscan', function(req, res) {
+    console.log('aliasscan(' + req.query.startName + ', ' + req.query.maxReturned + ')');
+    sysclient.aliasscan(req.query.startName, req.query.maxReturned, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
+router.get('/aliasupdate', function(req, res) {
+    console.log('aliasupdate(' + req.query.aliasName + ', ' + req.query.aliasValue + ', ' + req.query.toAddress + ')');
+    sysclient.aliasupdate(req.query.aliasName, req.query.aliasValue, req.query.toAddress, function(err, result, resHeaders) {
+        if(handleError(res, err)) return;
+
+        res.json(result);
+    });
+});
+
 //general functions
 function handleError(res, err) {
     if (err) {
