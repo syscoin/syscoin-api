0fcde0f1b4fc20f362714680e427b1208cf6a31e
Dan Wasyluk <dwasyluk@gmail.com>
2016-06-18 01:05:47 -0400

    Added config reading and basic security definitions/handling strategy. Server code for applying security to routes/passing from client not yet regenerated.

---
+++ server/nodejs/config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+//leave blank for default location of /root/.syscoin
+config.sys_location = process.env.SYS_LOCATION || "/root/.syscoin";
+
+config.api_secret = "iamapisecret";
+config.secure = process.env.SECURE || true;
+config.port = process.env.PORT || 8000;
+
+module.exports = config;
--- server/nodejs/index.js
+++ server/nodejs/index.js
@@ -5,18 +5,47 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var serverPort = 8000;
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
+//load external config
+var config = require('./config');
 var syscoin = require('syscoin');
+var lineReader = require('readline').createInterface({
+  input: fs.createReadStream(config.sys_location + "syscoin.conf")
+});
+
+var syscoinClient, rpcuser, rpcpass, rpcport;
+
+lineReader.on('line', function (line) {
+  if(line.indexOf('rpcuser') != -1) {
+    rpcuser = line.substr(line.indexOf('=') + 1);
+  }
+
+  if(line.indexOf('rpcpassword') != -1) {
+    rpcpass = line.substr(line.indexOf('=') + 1);
+  }
+
+  if(line.indexOf('rpcport') != -1) {
+    rpcport = line.substr(line.indexOf('=') + 1);
+  }
+});
+
+lineReader.on('close', function (line) {
+  console.log("RPCUSER:", rpcuser);
+  console.log("RPCPASS:", rpcpass);
+  console.log("RPCPORT:", rpcport);
+
+  syscoinClient = new syscoin.Client({
+    host: 'localhost',
+    port: rpcport,
+    user: rpcuser,
+    pass: rpcpass,
+    timeout: 30000
+  });
+});
 
 // all config options are optional
-exports.syscoinClient = new syscoin.Client({
-  host: 'localhost',
-  port: 8336,
-  user: 'username',
-  pass: 'password',
-  timeout: 30000
-});
+exports.syscoinClient = syscoinClient;
 
 // swaggerRouter configuration
 var options = {
@@ -35,6 +64,38 @@ app.use(function(req, res, next) {
   next();
 });
 
+// AUTH with JWT
+app.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, config.api_secret, function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+});
+
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
@@ -50,8 +111,8 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerUi());
 
   // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  http.createServer(app).listen(config.port, function () {
+    console.log('Your server is listening on port %d (http://localhost:%d)', config.port, config.port);
+    console.log('Swagger-ui is available on http://localhost:%d/docs', config.port);
   });
 });
