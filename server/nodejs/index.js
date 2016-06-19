c703d6a22fe908cc0a4629c6632206e39eecfe43
Dan Wasyluk <dwasyluk@gmail.com>
2016-06-19 01:32:36 -0400

    Added SHA1 auth validation to login. JWT generated on valid auth.

--- server/nodejs/index.js
+++ server/nodejs/index.js
@@ -5,7 +5,7 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var jwt    = require('jsonwebtoken');
 
 //load external config
 var config = require('./config');
@@ -14,7 +14,10 @@ var lineReader = require('readline').createInterface({
   input: fs.createReadStream(config.sys_location + "syscoin.conf")
 });
 
+
 var syscoinClient, rpcuser, rpcpass, rpcport;
+
+
 
 lineReader.on('line', function (line) {
   if(line.indexOf('rpcuser') != -1) {
@@ -42,78 +45,92 @@ lineReader.on('close', function (line) {
     pass: rpcpass,
     timeout: 30000
   });
+
+  exports.syscoinClient = syscoinClient;
+  exports.rpcuser = rpcuser
+  exports.rpcpass = rpcpass;
+
+  initSwagger();
+  initHttp();
 });
 
-// all config options are optional
-exports.syscoinClient = syscoinClient;
+var swaggerDoc, options;
+function initSwagger() {
+  // swaggerRouter configuration
+  options = {
+    swaggerUi: '/swagger.json',
+    controllers: './controllers',
+    useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+  };
 
-// swaggerRouter configuration
-var options = {
-  swaggerUi: '/swagger.json',
-  controllers: './controllers',
-  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
-};
+  // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+  var spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
+  swaggerDoc = jsyaml.safeLoad(spec);
+}
 
-// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
+function initHttp() {
 
-//CORS
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+  //CORS
+  app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  });
 
 // AUTH with JWT
-app.use(function(req, res, next) {
+  app.use(function(req, res, next) {
 
-  // check header or url parameters or post parameters for token
-  //var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  //
-  //// decode token
-  //if (token) {
-  //
-  //  // verifies secret and checks exp
-  //  jwt.verify(token, config.api_secret, function(err, decoded) {
-  //    if (err) {
-  //      return res.json({ success: false, message: 'Failed to authenticate token.' });
-  //    } else {
-  //      // if everything is good, save to request for use in other routes
-  //      req.decoded = decoded;
-  //      next();
-  //    }
-  //  });
-  //
-  //} else {
-  //
-  //  // if there is no token
-  //  // return an error
-  //  return res.status(403).send({
-  //    success: false,
-  //    message: 'No token provided.'
-  //  });
-  //
-  //}
-  next();
-});
-
-// Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
-
-  // Validate Swagger requests
-  app.use(middleware.swaggerValidator());
-
-  // Route validated requests to appropriate controller
-  app.use(middleware.swaggerRouter(options));
-
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());
-
-  // Start the server
-  http.createServer(app).listen(config.port, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', config.port, config.port);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', config.port);
+    // check header or url parameters or post parameters for token
+    //var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    //
+    //// decode token
+    //if (token) {
+    //
+    //  // verifies secret and checks exp
+    //  jwt.verify(token, config.api_secret, function(err, decoded) {
+    //    if (err) {
+    //      return res.json({ success: false, message: 'Failed to authenticate token.' });
+    //    } else {
+    //      // if everything is good, save to request for use in other routes
+    //      req.decoded = decoded;
+    //      next();
+    //    }
+    //  });
+    //
+    //} else {
+    //
+    //  // if there is no token
+    //  // return an error
+    //  return res.status(403).send({
+    //    success: false,
+    //    message: 'No token provided.'
+    //  });
+    //
+    //}
+    next();
   });
-});
+
+  // Initialize the Swagger middleware
+  swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    app.use(middleware.swaggerMetadata());
+
+    // Validate Swagger requests
+    app.use(middleware.swaggerValidator());
+
+    // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
+
+    // Serve the Swagger documents and Swagger UI
+    app.use(middleware.swaggerUi());
+
+    // Start the server
+    http.createServer(app).listen(config.port, function () {
+      console.log('Your server is listening on port %d (http://localhost:%d)', config.port, config.port);
+      console.log('Swagger-ui is available on http://localhost:%d/docs', config.port);
+    });
+  });
+}
+
+
+
+
