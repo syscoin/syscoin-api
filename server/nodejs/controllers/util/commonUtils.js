26d2b3ef126d2fe5ae06885d0d0957a9730717ea
dwasyluk <FUnstuff1>
2017-10-21 23:22:25 -0400

    Fixed error reporting in syscoin-core (inherited from bitcoin-core) to report verbose errors from core all the way top client rather than everything simply being "Error 500". Maintains 500 status but adds more robust info in error sent to client.

--- server/nodejs/controllers/util/commonUtils.js
+++ server/nodejs/controllers/util/commonUtils.js
@@ -4,6 +4,19 @@ function reportError(response, errorStr) {
   console.log("Error: " + errorStr);
   response.statusCode = 500;
 
+  //transform the RPC Error into a more Syscoin-specific error while maintaining the 500 status
+  const rpcErrorStr = "RpcError: 500 "; //with trailing space!
+  let errObjStartIndex = errorStr.toString().indexOf(rpcErrorStr);
+  let errObj;
+  if(errObjStartIndex >= 0) {
+    let errObjStr = errorStr.toString().substr(rpcErrorStr.length-1);
+    errObj = JSON.parse(errObjStr);
+  }
+
+  if(errObj) {
+    return response.end(`${errObj.code} - ${errObj.message}`);
+  }
+
   return response.end(JSON.stringify(errorStr.toString()));
 }
 
