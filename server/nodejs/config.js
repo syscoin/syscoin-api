0d6719cdfad1e103f42ffd7273be6a7c424a16c9
dwasyluk <FUnstuff1>
2017-10-31 15:15:36 -0400

    Bump syscoin-core version to latest to try and fix build issue

--- server/nodejs/config.js
+++ server/nodejs/config.js
@@ -4,7 +4,7 @@ var config = {};
 config.sys_location = process.env.SYS_LOCATION || "/root/.syscoin/";
 
 config.debugEnabled = process.env.DEBUG || false;
-config.methodsWithLoggingDisabled = JSON.parse(process.env.METHODS_WITH_LOGGING_DISABLED) || [];
+config.methodsWithLoggingDisabled = process.env.METHODS_WITH_LOGGING_DISABLED ? JSON.parse(process.env.METHODS_WITH_LOGGING_DISABLED) : [];
 
 config.api_secret = "iamapisecret";
 config.secure = process.env.SECURE || true;
