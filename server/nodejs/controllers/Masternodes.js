0b7cdec1b00c69d1e9c30541c29c554d32b74df1
Ahmer Yasin <ahmeryasin1992@gmail.com>
2018-03-15 02:19:36 +0500

    add new api

--- server/nodejs/controllers/Masternodes.js
+++ server/nodejs/controllers/Masternodes.js
@@ -6,10 +6,14 @@ var url = require('url');
 var MasterNode = require('./MasternodesService');
 
 
-module.exports.sentinelping = function addmultisigaddress (req, res, next) {
+module.exports.sentinelping = function sentinelping (req, res, next) {
     MasterNode.sentinelping(req.swagger.params, res, next);
 };
 
-module.exports.voteraw = function addmultisigaddress (req, res, next) {
+module.exports.voteraw = function voteraw (req, res, next) {
     MasterNode.voteraw(req.swagger.params, res, next);
+};
+
+module.exports.privatesend = function privatesend (req, res, next) {
+    MasterNode.privatesend(req.swagger.params, res, next);
 };
--- server/nodejs/controllers/MasternodesService.js
+++ server/nodejs/controllers/MasternodesService.js
@@ -57,3 +57,34 @@ exports.voteraw = function(args, res, next) {
     var arr = varUtils.getArgsArr(argList, args, "POST", cb);
     syscoinClient.voteraw.apply(syscoinClient, arr);
 };
+
+
+exports.privatesend = function(args, res, next) {
+    /**
+     * parameters expected in the args:
+     * request (privatesend)
+     **/
+    var argList = [
+        { prop: "masternode-tx-hash" },
+        { prop: "masternode-tx" },
+        { prop: "governance-hash" },
+        { prop: "vote-signal" },
+        { prop: "vote-outcome" },
+        { prop: "time" },
+        { prop: "vote-sig" }
+    ];
+
+    var cb = function(err, result, resHeaders) {
+        res.setHeader('Content-Type', 'application/json');
+
+        if (err) {
+            return commonUtils.reportError(res, err);
+        }
+
+        commonUtils.log('privatesend', result, "privatesend");
+        res.end(JSON.stringify(result));
+    };
+
+    var arr = varUtils.getArgsArr(argList, args, "POST", cb);
+    syscoinClient.voteraw.apply(syscoinClient, arr);
+};
