ee0b3da86a9f89ba31410819955f97d5ecee007b
Justice Gray <justice.gray@gmail.com>
2018-04-13 11:27:35 -0700

    Small clarity changes.

--- server/nodejs/config.js
+++ server/nodejs/config.js
@@ -2,7 +2,7 @@ var config = {};
 
 var defaultSyscoinConfigLocation = "/root/.syscoin/"
 
-var setupIntegratedMode = (envArgument, envArgumentIdentifier, preamble, friendlyModeName) => {
+var setupInMode = (envArgument, envArgumentIdentifier, preamble, friendlyModeName) => {
     console.log(`Running Syscoin API in '${friendlyModeName}' API run mode.\n${preamble}`);
     if (envArgument) {
         config.sys_location = envArgument;
@@ -10,24 +10,24 @@ var setupIntegratedMode = (envArgument, envArgumentIdentifier, preamble, friendl
     }
     else {
         config.sys_location = defaultSyscoinConfigLocation;
-        console.log(`Warning: SYSCOIN_CONFIG_ISOLATED_LOCATION was not set.  Check your .env file!`);
+        console.log(`Warning: ${envArgumentIdentifier} was not set.  Check your .env file!`);
         console.log(`Using default syscoin.conf path of '${defaultSyscoinConfigLocation}'`)
     }
 };
 
 if (!process.env.APIRUNMODE) {
     console.log(`Running Syscoin API without setting APIRUNMODE (should be 'isolated' or 'integrated').  Attempting to run in integrated mode.`);
-    setupIntegratedMode(process.env.SYSCOIN_CONFIG_INTEGRATED_LOCATION, 
+    setupInMode(process.env.SYSCOIN_CONFIG_INTEGRATED_LOCATION, 
         "SYSCOIN_CONFIG_INTEGRATED_LOCATION",
         `This is intended to be running off of an integrated instance (normally an official release) of syscoin.`,
         `integrated`);
 }
 else if (process.env.APIRUNMODE === 'isolated') {
-    setupIntegratedMode(process.env.SYSCOIN_CONFIG_ISOLATED_LOCATION, "SYSCOIN_CONFIG_ISOLATED_LOCATION",
+    setupInMode(process.env.SYSCOIN_CONFIG_ISOLATED_LOCATION, "SYSCOIN_CONFIG_ISOLATED_LOCATION",
     `This is intended to be running off of an isolated instance (possibly custom build) of syscoin.`,`isolated`);
 }
 else if (process.env.APIRUNMODE === 'integrated') {
-    setupIntegratedMode(process.env.SYSCOIN_CONFIG_INTEGRATED_LOCATION, "SYSCOIN_CONFIG_INTEGRATED_LOCATION",
+    setupInMode(process.env.SYSCOIN_CONFIG_INTEGRATED_LOCATION, "SYSCOIN_CONFIG_INTEGRATED_LOCATION",
     `This is intended to be running off of an integrated instance (normally an official release) of syscoin.`,`integrated`);
 }
 
