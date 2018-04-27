a23c424d87e09720052203ff090514d5819cf8c0
Justice Gray <justice.gray@gmail.com>
2018-04-26 21:47:27 -0700

    Naming changes + method generator for ditching the repetitive code post 3.0.

--- server/nodejs/controllers/Asset.js
+++ server/nodejs/controllers/Asset.js
@@ -1,38 +1,41 @@
 'use strict';
 
 var url = require('url');
-var Aliases = require('./AssetService');
+var Assets = require('./AssetService');
 
 module.exports.assetinfo = function assetinfo (req, res, next) {
-    Aliases.assetinfo(req.swagger.params, res, next);
+    Assets.assetinfo(req.swagger.params, res, next);
   };
   
   module.exports.assetsend = function assetsend (req, res, next) {
-    Aliases.assetsend(req.swagger.params, res, next);
+    Assets.assetsend(req.swagger.params, res, next);
   };
   
   module.exports.assetnew = function assetnew (req, res, next) {
-    Aliases.assetnew(req.swagger.params, res, next);
+    Assets.assetnew(req.swagger.params, res, next);
   };
   
   module.exports.assetallocationinfo = function assetallocationinfo (req, res, next) {
-    Aliases.assetallocationinfo(req.swagger.params, res, next);
+    Assets.assetallocationinfo(req.swagger.params, res, next);
   };
   
   module.exports.assetallocationsend = function assetallocationsend (req, res, next) {
-    Aliases.assetallocationsend(req.swagger.params, res, next);
+    Assets.assetallocationsend(req.swagger.params, res, next);
   };
   
   module.exports.assetallocationsenderstatus = function assetallocationsenderstatus (req, res, next) {
-    Aliases.assetallocationsenderstatus(req.swagger.params, res, next);
+    Assets.assetallocationsenderstatus(req.swagger.params, res, next);
   };
   
   module.exports.assettransfer = function assettransfer (req, res, next) {
-    Aliases.assettransfer(req.swagger.params, res, next);
+    Assets.assettransfer(req.swagger.params, res, next);
   };
   
   module.exports.assetupdate = function assetupdate (req, res, next) {
-    Aliases.assetupdate(req.swagger.params, res, next);
+    Assets.assetupdate(req.swagger.params, res, next);
   };
 
-  //missing assetallocationcollectinterest
+  module.exports.assetallocationcollectinterest = function assetallocationcollectinterest (req, res, next) {
+    Assets.assetallocationcollectinterest(req.swagger.params, res, next);
+  }
+  
---
+++ server/nodejs/controllers/util/methodGenerator.js
@@ -0,0 +1,26 @@
+var syscoinClient = require('../../index').syscoinClient;
+var varUtils = require('./varUtils');
+var commonUtils = require('./commonUtils');
+
+function generateGenericSyscoinMethod(argList, syscoinMethod, syscoinMethodName, httpMethod) {
+    return function(args, rest, next) {
+        if (!syscoinMethod) {
+            throw Error(`${syscoinMethodName} was called bu not present.  
+            Check your syscoin-core definitions to see whether it exists.`);
+        }
+        let callback = function(err, result, resHeaders) {
+            res.setHeader('Content-Type', 'application/json');
+    
+            if (err) {
+                return commonUtils.reportError(res, err);
+            }
+    
+            console.log(`${syscoinMethodName}: ${result}`);
+            res.end(JSON.stringify(result));
+        };
+        let arr = varUtils.getArgsArr(argList,args,httpMethod,callback);
+        syscoinMethod.apply(syscoinClient,arr);
+    }
+} 
+
+module.exports.generateGenericSyscoinMethod = generateGenericSyscoinMethod;
