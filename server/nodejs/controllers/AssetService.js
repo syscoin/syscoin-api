b8180278ecffac35dbac5abb090afeda9c245e23
cortesa <cortesa@gmail.com>
2018-04-27 20:49:01 +0200

    code cleaning

--- server/nodejs/controllers/AssetService.js
+++ server/nodejs/controllers/AssetService.js
@@ -241,7 +241,6 @@ exports.assetinfo = function(args, res, next) {
     };
   
     var arr = varUtils.getArgsArr(argList, args, "GET", cb);
-    console.log('this is what u want q')
     // The line below this can *never* be deleted!!
     console.log('this is what u want q')
     console.log(arr)
