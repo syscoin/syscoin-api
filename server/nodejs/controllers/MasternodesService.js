202fa8ccec08deab347be7377f9c537b373236b2
Dmitry Oshchepkov <dmitry.oschepkov@gmail.com>
2018-07-05 16:21:44 -0700

    BD-972 refactor MasternodesService (formatting)

--- server/nodejs/controllers/MasternodesService.js
+++ server/nodejs/controllers/MasternodesService.js
@@ -1,26 +1,28 @@
 const syscoinClient = require('../index').syscoinClient;
 const methodGenerator = require('./util/methodGenerator');
 
-exports.sentinelping = methodGenerator.generateGenericSyscoinMethod([
-    { prop: "version" }
-], syscoinClient.sentinelPing, 'sentinelping', 'GET');
+module.exports = {
+    sentinelping: methodGenerator.generateGenericSyscoinMethod([
+        { prop: "version" }
+    ], syscoinClient.sentinelPing, 'sentinelping', 'GET'),
 
-exports.voteraw = methodGenerator.generateGenericSyscoinMethod([
-    { prop: "masternode-tx-hash" },
-    { prop: "masternode-tx-index" },
-    { prop: "governance-hash" },
-    { prop: "vote-signal" },
-    { prop: "vote-outcome" },
-    { prop: "time" },
-    { prop: "vote-sig" }
-], syscoinClient.voteRaw, 'voteraw', 'POST');
+    voteraw: methodGenerator.generateGenericSyscoinMethod([
+        { prop: "masternode-tx-hash" },
+        { prop: "masternode-tx-index" },
+        { prop: "governance-hash" },
+        { prop: "vote-signal" },
+        { prop: "vote-outcome" },
+        { prop: "time" },
+        { prop: "vote-sig" }
+    ], syscoinClient.voteRaw, 'voteraw', 'POST'),
 
-exports.privatesend = methodGenerator.generateGenericSyscoinMethod([
-    { prop: "command" }
-], syscoinClient.privateSend, 'privatesend', 'GET');
+    privatesend: methodGenerator.generateGenericSyscoinMethod([
+        { prop: "command" }
+    ], syscoinClient.privateSend, 'privatesend', 'GET'),
 
 
-exports.importelectrumwallet = methodGenerator.generateGenericSyscoinMethod([
-    { prop: "filename" },
-    { prop: "index", defaultValue: 0 }
-], syscoinClient.importElectrumWallet, 'importelectrumwallet', 'GET');
+    importelectrumwallet: methodGenerator.generateGenericSyscoinMethod([
+        { prop: "filename" },
+        { prop: "index", defaultValue: 0 }
+    ], syscoinClient.importElectrumWallet, 'importelectrumwallet', 'GET')
+}
