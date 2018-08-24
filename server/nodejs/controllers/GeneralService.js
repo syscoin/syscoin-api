8b9295c6a1753c110c7bf05d2213269adf3027cc
evscott <eliotvscott@gmail.com>
2018-08-24 12:06:35 -0700

    Added syscoin-cli command 'getdifficulty'

--- server/nodejs/controllers/GeneralService.js
+++ server/nodejs/controllers/GeneralService.js
@@ -95,6 +95,10 @@ module.exports = {
     syscoinClient.getInfo.apply(syscoinClient, arr);
   },
 
+  getdifficulty: methodGenerator.generateGenericSyscoinMethod(
+    [],
+    syscoinClient.getdifficulty, 'getdifficulty', 'GET'),
+
   getmininginfo: methodGenerator.generateGenericSyscoinMethod(
     [],
     syscoinClient.getMiningInfo, 'getmininginfo', 'GET'),
